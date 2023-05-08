6a0187dfa8d87eda095370ac34a624c079432912
hoomili <hasadian@hotmail.com>
2023-05-08 11:05:32 -0700

    added sample data files

---
+++ db/seeds/03_favourite_maps.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, map_id) VALUES (1, 1);
+INSERT INTO users (user_id, map_id) VALUES (1, 2);
+INSERT INTO users (user_id, map_id) VALUES (1, 4);
+INSERT INTO users (user_id, map_id) VALUES (1, 6);
+INSERT INTO users (user_id, map_id) VALUES (2, 3);
+INSERT INTO users (user_id, map_id) VALUES (2, 6);
