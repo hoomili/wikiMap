9a4d3f5b7e7f8f7d4c02bae7127bc95ed2400a17
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-05-08 13:17:21 -0700

    Create getMaps functions to return all data from maps table

---
+++ db/queries/maps.js
@@ -0,0 +1,9 @@
+const db = require("../connection");
+
+const getMaps = () => {
+  return db.query("SELECT * FROM maps;").then((data) => {
+    return data.rows;
+  });
+};
+
+module.exports = { getMaps };
