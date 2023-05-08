ec7ac3e24c9a320bf72c653a7443bf5a038d814d
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-05-08 11:06:19 -0700

    Add 3rd user

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,2 +1,3 @@
 INSERT INTO users (name, username, email, password) VALUES ('Alice', 'Alice', 'alice@test.com', '123');
 INSERT INTO users (name, username, email, password) VALUES ('Kira', 'Kira', 'kira@test.com', '456');
+INSERT INTO users (name, username, email, password) VALUES ('John', 'John', 'john@test.com', '789');
