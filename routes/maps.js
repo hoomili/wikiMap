0af87f62a0babfbb1b170898cf69611f483978f3
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-05-08 13:55:26 -0700

    Fix maps route to receive all data from maps table

--- routes/maps.js
+++ routes/maps.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const mapQueries = require("../db/queries/maps");
 
 router.get("/", (req, res) => {
-  res.send("This is maps page");
+  mapQueries
+    .getMaps()
+    .then((maps) => {
+      res.json({ maps });
+      res.send("This is maps page");
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
 
 module.exports = router;
