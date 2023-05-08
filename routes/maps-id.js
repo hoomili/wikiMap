0291adc2c8312a78b69165b20e128961a367423a
hoomili <hasadian@hotmail.com>
2023-05-08 16:43:45 -0700

    routes renders the page with map and pin data

---
+++ routes/maps-id.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const mapsQueries = require('../db/queries/maps');
+const pinsQueries = require('../db/queries/pins');
+
+router.get('/', (req, res) => {
+  let templateVar = {};
+  mapsQueries.getMapData(req.params.id)
+    .then(map => {
+      templateVar.map = map[0];
+      return pinsQueries.getPinsData(map[0].id);
+
+    })
+    .then(pins => {
+      templateVar.pins = pins;
+      res.render('../views/pages/map', templateVar);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+module.exports = router;
