b1635d9fc0baee8e2f8c4850d8435f8b72e8671c
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-05-09 16:22:06 -0700

    Update getMaps function

--- routes/maps-id.js
+++ routes/maps-id.js
@@ -1,25 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router({ mergeParams: true });
-const mapsQueries = require('../db/queries/maps');
-const pinsQueries = require('../db/queries/pins');
+const mapsQueries = require("../db/queries/maps");
+const pinsQueries = require("../db/queries/pins");
+const newMapQueries = require("../db/queries/new-map");
 
-router.get('/', (req, res) => {
-  let templateVar = {ApiKey: process.env.API_KEY};
-  mapsQueries.getMapData(req.params.id)
-    .then(map => {
+router.get("/", (req, res) => {
+  let templateVar = { ApiKey: process.env.API_KEY };
+  mapsQueries
+    .getMapData(req.params.id)
+    .then((map) => {
       templateVar.map = map[0];
       return pinsQueries.getPinsData(map[0].id);
-
     })
-    .then(pins => {
+    .then((pins) => {
       templateVar.pins = pins;
-      res.render('../views/pages/map', templateVar);
+      res.render("../views/pages/map", templateVar);
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
     });
 });
+
+router.post("/", (req, res) => {
+  const mapTitle = req.body.title;
+  const mapImage = req.body.image;
+  const mapCity = req.body.city;
+
+  newMapQueries.addNewMap(mapTitle, mapCity, mapImage);
+});
+
+// newMapQueries
+//   .addNewMap(req.params.id)
+//   .then((map) => {
+//     templateVar.map = map[0];
+//     return newMapQueries.addNewMap(map[0].id);
+//   })
+//   .then((pins) => {
+//     templateVar.pins = pins;
+//     res.render("../views/pages/map");
+//   })
+//   .catch((err) => {
+//     res.status(500).json({ error: err.message });
+//   });
+// res.render("../views/pages/map");
 
 module.exports = router;
