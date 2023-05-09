a65d29f93fcd3888a07f4cc10653bcc5ce3613ce
Adam McPhee <adkmcphee@gmail.com>
2023-05-09 02:01:45 +0000

    add contributions section

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const router  = express.Router();
-const {getFavouriteMaps} = require('../db/queries/favourites')
+const express = require("express");
+const router = express.Router();
+const { getFavouriteMaps } = require("../db/queries/favourites");
+const { getMyContributions } = require("../db/queries/contributions");
 
-
-router.get('/:id', (req, res) => {
-
-
-getFavouriteMaps(req.params.id)
-  .then(data => {
-    const templateVars = {
-      data: data
-    };
-    res.render('pages/profile', templateVars)
-  })
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  getFavouriteMaps(id)
+    .then((favourites) => {
+      return getMyContributions(id).then((contributions) => {
+        const templateVars = {
+          favourites: favourites,
+          contributions: contributions,
+        };
+        res.render("pages/profile", templateVars);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error 500");
+    });
 });
-
 
 module.exports = router;
