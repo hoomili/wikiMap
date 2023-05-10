b26c67916d810c126215c0c9829d01d12cc5ae33
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-05-10 11:30:48 -0700

    Show login button when no cookie is set

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,6 @@ const router = express.Router();
 router.get("/:id", (req, res) => {
   const userId = req.params.id;
   res.cookie("user_id", userId);
-
   res.redirect(`/profiles/${userId}`);
 });
 
--- routes/new-map.js
+++ routes/new-map.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const newMapQueries = require("../db/queries/new-map");
 
 router.get("/", (req, res) => {
-  res.render("pages/new-map");
+  const userId = req.cookies.user_id;
+  const templateVars = {
+    userId,
+  };
+
+  res.render("pages/new-map", templateVars);
 });
 
 module.exports = router;
