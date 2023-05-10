59d6bc6e1668f47865b0295b9951bba0b8fa8a3e
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-05-10 14:50:53 -0700

    Remove console.log

--- routes/login.js
+++ routes/login.js
@@ -4,8 +4,6 @@ const router = express.Router();
 router.get("/:id", (req, res) => {
   const userId = req.params.id;
   res.cookie("user_id", userId);
-
-  // let user = req.cookies.user_id;
   res.redirect(`/profiles/${userId}`);
 });
 
