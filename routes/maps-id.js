5366106d3a3dee8c48d5ca291a09a2720fac9c10
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-05-09 18:30:03 -0700

    Create route for create new map POST request

--- routes/maps-id.js
+++ routes/maps-id.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const mapsQueries = require("../db/queries/maps");
 const pinsQueries = require("../db/queries/pins");
-const newMapQueries = require("../db/queries/new-map");
 
 router.get("/", (req, res) => {
   let templateVar = { ApiKey: process.env.API_KEY };
@@ -20,36 +19,5 @@ router.get("/", (req, res) => {
       res.status(500).json({ error: err.message });
     });
 });
-
-router.post("/", (req, res) => {
-  const mapTitle = req.body.title;
-  const mapImage = req.body.image;
-  const mapCity = req.body.city;
-
-  newMapQueries
-    .addNewMap(mapTitle, mapCity, mapImage)
-    .then(() => {
-      res.redirect("/maps");
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error 500");
-    });
-});
-
-// newMapQueries
-//   .addNewMap(req.params.id)
-//   .then((map) => {
-//     templateVar.map = map[0];
-//     return newMapQueries.addNewMap(map[0].id);
-//   })
-//   .then((pins) => {
-//     templateVar.pins = pins;
-//     res.render("../views/pages/map");
-//   })
-//   .catch((err) => {
-//     res.status(500).json({ error: err.message });
-//   });
-// res.render("../views/pages/map");
 
 module.exports = router;
